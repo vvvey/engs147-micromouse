2e03961f82173b049c5949cae1f3f3588dc8d574
vvvey <vuthyvey25@gmail.com>
2025-05-08 12:55:17 -0400

    Update the Encoder file

--- src/Encoder/Encoder.cpp
+++ src/Encoder/Encoder.cpp
@@ -1,19 +1,39 @@
-#include <AxisEncoderShield3.h>    
-class Encoder {
-    public:
-        Encoder(int id, float ticks_per_rev);
-    
-        void begin();
-        void update(unsigned long current_time_ms);  // pass millis()
-    
-        float getVelocity();    // returns omega (rad/s)
-        long getPosition();     // encoder ticks
-        void reset();
-    
-    private:
-        int encoder_id;
-        float ticks_per_rev;
-        long prev_ticks;
-        unsigned long prev_time_ms;
-        float omega_rad_s;
-};
+#include "Encoder.h"
+#include <AxisEncoderShield3.h>
+
+Encoder::Encoder(int id, float ticks_per_rev) : encoder_id(id), ticks_per_rev(ticks_per_rev), prev_ticks(0), prev_time_ms(0), omega_rad_s(0) {}
+
+void Encoder::begin() {
+    initEncoderShield();
+    prev_ticks = getEncoderValue(encoder_id);
+    prev_time_ms = millis();
+    init_ticks = prev_ticks;
+}
+
+void Encoder::update(unsigned long current_time_ms) {
+    long curr_ticks = getEncoderValue(encoder_id);
+    unsigned long dt_ms = current_time_ms - prev_time_ms;
+
+    if (dt_ms > 0) {
+        float dt_sec = dt_ms / 1000.0;
+        float revs = (curr_ticks - prev_ticks - init_ticks) / ticks_per_rev;
+        omega_rad_s = (revs * 2.0 * PI) / dt_sec;
+
+        prev_ticks = curr_ticks;
+        prev_time_ms = current_time_ms;
+    }
+}
+
+float Encoder::getVelocity() {
+    return omega_rad_s;
+}
+
+long Encoder::getPosition() {
+    return getEncoderValue(encoder_id);
+}
+
+void Encoder::reset() {
+    init_ticks = prev_ticks;
+    omega_rad_s = 0;
+    prev_time_ms = millis();
+}
