8b0b5bbb14870c78a78c2c2f7ff3aad28e348d8b
JSapun <sapun.justin@gmail.com>
2025-05-31 21:09:30 -0400

    added leds and button for floodfill debug

--- src/floodfill_main.cpp
+++ src/floodfill_main.cpp
@@ -16,6 +16,14 @@
 #define LOG_BTN 30
 #define STOP_BTN 32
 
+#define LED_LEFT   29   
+#define LED_FRONT  31    
+#define LED_RIGHT  33   // Green
+#define CONTINUE_BTN 25 
+
+
+void showWallsAndWait(WallReading w);
+
 MotionController motion;
 
 // Maze position & direction tracking
@@ -25,7 +33,12 @@ int direction = NORTH;
 
 bool run = false;
 
+
 void setup() {
+    digitalWrite(LED_LEFT, HIGH);
+    digitalWrite(LED_FRONT, HIGH);
+    digitalWrite(LED_RIGHT, HIGH);
+
     Serial.begin(115200);
     IMU_init();
     TOF_init();
@@ -37,11 +50,19 @@ void setup() {
     pinMode(LOG_BTN, INPUT_PULLUP);
     pinMode(STOP_BTN, INPUT_PULLUP);
 
+    pinMode(LED_FRONT, OUTPUT);
+    pinMode(LED_LEFT, OUTPUT);
+    pinMode(LED_RIGHT, OUTPUT);
+    pinMode(CONTINUE_BTN, INPUT_PULLUP);
+
     initializeMaze();
     initializeFloodfill();
 
     delay(1000);
     Serial.println("Ready");
+    digitalWrite(LED_LEFT, LOW);
+    digitalWrite(LED_FRONT, LOW);
+    digitalWrite(LED_RIGHT, LOW);
 }
 
 void loop() {
@@ -64,6 +85,9 @@ void loop() {
         // Step 1: Sense and update wall map
         WallReading walls = readWalls(curRow, curCol, direction);
         updateWallMap(curRow, curCol, walls, direction);
+
+        // Step 1.5: Debugging (Real time understanding of robot's perception)
+        showWallsAndWait(walls);
 
         // Step 2: Floodfill map
         floodfill();
@@ -93,4 +117,16 @@ void loop() {
             run = false;
         }
     }
+}
+
+
+void showWallsAndWait(WallReading w) {
+    digitalWrite(LED_FRONT, w.front ? HIGH : LOW);
+    digitalWrite(LED_LEFT,  w.left  ? HIGH : LOW);
+    digitalWrite(LED_RIGHT, w.right ? HIGH : LOW);
+
+    while (digitalRead(CONTINUE_BTN) == HIGH) {
+        delay(10);
+    }
+    delay(500);
 }
